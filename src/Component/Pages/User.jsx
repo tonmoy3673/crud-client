50785d62a2e8ea77cfbe2257f8bf47bd7d409873
tonmoy <tonmoy3673@gmail.com>
2024-11-14 21:28:57 +0600

    :pencil: create users files

---
+++ src/Component/Pages/User.jsx
@@ -0,0 +1,9 @@
+const User =()=>{
+    return (
+        <div>
+            <h2>User</h2>
+        </div>
+    )
+};
+
+export default User;
---
+++ src/Component/Pages/userDetails.jsx
@@ -0,0 +1,10 @@
+
+const UserDetails = () => {
+    return (
+        <div>
+            <h3>This is User Details Page</h3>
+        </div>
+    );
+};
+
+export default UserDetails;
