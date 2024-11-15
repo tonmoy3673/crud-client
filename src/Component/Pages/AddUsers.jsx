366b2b882c0f8a12895fac690794a8d387dadbdd
tonmoy <tonmoy3673@gmail.com>
2024-11-15 23:34:24 +0600

    :pencil: create submit button

--- src/Component/Pages/AddUsers.jsx
+++ src/Component/Pages/AddUsers.jsx
@@ -2,7 +2,7 @@ const AddUsers =()=>{
     return (
         <div className="bg-slate-200 py-8 md:py-24 flex justify-center items-center flex-col md:flex-row">
            <div className="ml-4">
-           <h2 className="text-xl md:text-3xl font-semibold mb-3 md:mb-0">Add New User</h2>
+           <h2 className="text-xl md:text-3xl font-semibold mb-3 md:mb-0 text-blue-600">Add New User</h2>
            </div>
             
                 <form  className="px-20">
@@ -24,9 +24,14 @@ const AddUsers =()=>{
                         <input className="py-1 px-2 rounded-md ml-3" type="text" id="photo" placeholder="Enter photo URL">
 
                         </input>
+                        
                     </div>
-            
-                    
+                        {/* ================= submit button =========== */}
+                    <div className="mt-3 md:mt-5 text-center">
+                    <button className="bg-blue-600 hover:bg-amber-500 px-2 py-1 text-base text-white hover:text-black rounded-md">
+                        Submit 
+                    </button>
+                    </div>
                     </div>
                 </form>
             
