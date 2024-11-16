a99649668a596143aff06ed8667c714aa4ca1cb2
tonmoy <tonmoy3673@gmail.com>
2024-11-16 21:30:09 +0600

    :bulb: display users

--- src/Component/Pages/User.jsx
+++ src/Component/Pages/User.jsx
@@ -1,8 +1,20 @@
-const User =()=>{
+const User =({user})=>{
+    const {name,email,photo} =user;
     return (
-        <div>
-            <h2>User</h2>
+       <div>
+         <div className="flex items-center">
+            <div>
+            <img src={photo}></img>
+           </div>
+            <div>
+            <h2 className="text-lg font-semibold">User Name : {name}</h2>
+            <h2 className="mt-2">User Email : <span className="text-blue-500 underline cursor-pointer">
+            {email}</span></h2>
+            </div>
+           
         </div>
+            
+       </div>
     )
 };
 
