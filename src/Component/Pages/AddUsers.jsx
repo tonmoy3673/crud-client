0d220ce266ae87997efbef4c9c40e34644660746
tonmoy <tonmoy3673@gmail.com>
2024-11-16 18:55:48 +0600

    :bulb: set submit button

--- src/Component/Pages/AddUsers.jsx
+++ src/Component/Pages/AddUsers.jsx
@@ -1,11 +1,27 @@
 const AddUsers =()=>{
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        console.log('clicked');
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const photo = form.photo.value;
+
+        const user ={
+            name,email,photo
+        };
+        console.log(user);
+        
+    }
+
     return (
         <div className="bg-slate-200 py-8 md:py-24 flex justify-center items-center flex-col md:flex-row">
            <div className="ml-4">
            <h2 className="text-xl md:text-3xl font-semibold mb-3 md:mb-0 text-blue-600">Add New User</h2>
            </div>
             
-                <form  className="px-20">
+                <form onSubmit={handleSubmit} className="px-20">
                     <div>
                     <div className="mb-6 flex flex-col md:flex-row items-center">
                         <label className="text-center" htmlFor="name">Name :</label>
