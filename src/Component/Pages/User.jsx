166fd67bcec92a419680937e93a6b4945ab9cf38
tonmoy <tonmoy3673@gmail.com>
2024-11-17 23:12:24 +0600

    :pencil: added icons

--- src/Component/Pages/User.jsx
+++ src/Component/Pages/User.jsx
@@ -1,3 +1,6 @@
+import { BsCartXFill, BsEyeFill, BsPencilFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
 const User =({user})=>{
     const {name,email,photo} =user;
     return (
@@ -11,9 +14,21 @@ const User =({user})=>{
             <h2 className="mt-2">Email : <span className="text-blue-500 underline cursor-pointer">
             {email}</span></h2>
             </div>
+            <div className="flex items-end justify-center flex-col gap-y-4 ms-6">
+                <Link>
+                <BsEyeFill />
+                </Link>
+                <Link>
+                <BsPencilFill />
+                </Link>
+                <Link>
+                <BsCartXFill />
+                </Link>
+            </div>
+           
            
         </div>
-
+       
        </div>
     )
 };
