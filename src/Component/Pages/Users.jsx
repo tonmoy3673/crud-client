6f45f8652fa8c7064a95e122b143ccc7978f84e0
tonmoy <tonmoy3673@gmail.com>
2024-11-19 23:03:29 +0600

    :bulb: implement show more button

--- src/Component/Pages/Users.jsx
+++ src/Component/Pages/Users.jsx
@@ -4,35 +4,49 @@ import User from "./User";
 
 const Users = () => {
   const [allUsers,setAllUsers]= useState([]);
+  const [length,setLength] = useState(3);
+  const [loading,setLoading] = useState(false)
   const users = useLoaderData();
   
 
   useEffect(()=>{
-    setAllUsers (users)
+    setAllUsers(users)
+    
     
   },[users]);
 
 const handleDelete =(id)=>{
+  setLoading(true)
   console.log(id);
   fetch(`http://localhost:5000/users/${id}`,{
     method:'DELETE',
     headers:{
       'content-type': 'application/json'
     },
-    body: JSON.stringify(allUsers)
+    
   })
   .then((res)=>res.json())
-  .then(()=>{setAllUsers(allUsers.filter((user)=> user._id !== id))})
-
+  .then((data)=>setAllUsers(data))
+  setLoading(false)
 }
 
 
   return (
     <div>
       <h2 className="text-center py-8 text-2xl font-semibold">All Users</h2>
+      <>
+     { loading && <p>Loading ............</p>}
+      </>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {
-            users && allUsers.map((user)=><User handleDelete={handleDelete} key={user._id} user={user}/>)
+            allUsers && allUsers.slice(0,length).map((user)=><User handleDelete={handleDelete} key={user._id} user={user}/>)
+        }
+      </div>
+      <div className="py-8 text-center">
+        {
+         length && length < allUsers.length ? <button onClick={()=>setLength(length+3)}>
+          Show More
+        </button> : "No Data Available"
         }
       </div>
     </div>
